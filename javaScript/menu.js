6926d7eeaff634cc33a53bb0b259ce65437f3cf5
xtoshie <wtadatochii@gmail.com>
2025-10-02 22:37:15 -0300

    v1.0.2 adicionado mais sobre o Elo Social, criação do menu.html, meni.js e linkando o menu js à todas as paginas

---
+++ javaScript/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("menu.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("menu").innerHTML = data;
+    })
+    .catch(error => console.error("Erro ao carregar o menu:", error));
+});
